d32d1bdfaf32fe678e9a9351a242a6813c519b76
Rasmus <Rasmus-nohr@hotmail.com>
2017-03-21 11:02:43 +0100

    Cleaned code up a bit and added commets.

--- DbGraf2/Controllers/MeteorologiDatasController.cs
+++ DbGraf2/Controllers/MeteorologiDatasController.cs
@@ -14,159 +14,42 @@ namespace DbGraf2.Controllers
 {
     public class MeteorologiDatasController : Controller
     {
+        // Our db context
         private VillumResearchXMLEntities db = new VillumResearchXMLEntities();
 
+        // The chart containing our data
         public static Chart Chart { get; set; }          
 
-        // GET: MeteorologiDatas
+        // Index shows our chart
         public ActionResult Index()
         {
+            // ViewModel containing data for the table.
             var viewModel = new ChartViewModel();
 
-            viewModel.Data = db.MeteorologiData.Take(500).ToList();
+            // Get table data from the db.
+            viewModel.Data = db.MeteorologiData.Take(100).ToList();
+
+            // Show the data in the view
             return View(viewModel);
         }
 
-        //// GET: MeteorologiDatas/Details/5
-        //public ActionResult Details(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    MeteorologiData meteorologiData = db.MeteorologiData.Find(id);
-        //    if (meteorologiData == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(meteorologiData);
-        //}
-
-        //// GET: MeteorologiDatas/Create
-        //public ActionResult Create()
-        //{
-        //    return View();
-        //}
-
-        //// POST: MeteorologiDatas/Create
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Create([Bind(Include = "Id,StartDateTime,WindDirection,WindSpeed,Temperature,Humidity,Radiation,Pressure")] MeteorologiData meteorologiData)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.MeteorologiData.Add(meteorologiData);
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-
-        //    return View(meteorologiData);
-        //}
-
-        //// GET: MeteorologiDatas/Edit/5
-        //public ActionResult Edit(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    MeteorologiData meteorologiData = db.MeteorologiData.Find(id);
-        //    if (meteorologiData == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(meteorologiData);
-        //}
-
-        //// POST: MeteorologiDatas/Edit/5
-        //// To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        //// more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        //[HttpPost]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult Edit([Bind(Include = "Id,StartDateTime,WindDirection,WindSpeed,Temperature,Humidity,Radiation,Pressure")] MeteorologiData meteorologiData)
-        //{
-        //    if (ModelState.IsValid)
-        //    {
-        //        db.Entry(meteorologiData).State = EntityState.Modified;
-        //        db.SaveChanges();
-        //        return RedirectToAction("Index");
-        //    }
-        //    return View(meteorologiData);
-        //}
-
-        //// GET: MeteorologiDatas/Delete/5
-        //public ActionResult Delete(int? id)
-        //{
-        //    if (id == null)
-        //    {
-        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-        //    }
-        //    MeteorologiData meteorologiData = db.MeteorologiData.Find(id);
-        //    if (meteorologiData == null)
-        //    {
-        //        return HttpNotFound();
-        //    }
-        //    return View(meteorologiData);
-        //}
-
-        //// POST: MeteorologiDatas/Delete/5
-        //[HttpPost, ActionName("Delete")]
-        //[ValidateAntiForgeryToken]
-        //public ActionResult DeleteConfirmed(int id)
-        //{
-        //    MeteorologiData meteorologiData = db.MeteorologiData.Find(id);
-        //    db.MeteorologiData.Remove(meteorologiData);
-        //    db.SaveChanges();
-        //    return RedirectToAction("Index");
-        //}
-
-        public ActionResult CreateLineChartActionResult()
+        public ActionResult GetChart()
         {
-            
             var finalechart = Chart.GetBytes("png");
             return File(finalechart, "image/bytes");
         }
 
-        public void CreateChart()
-        {
-            var dataHent = db.MeteorologiData.Take(500);
-            var data1 = new List<DateTime?>();
-            var data2 = new List<double?>();
-            var data3 = new List<double?>();
-
-            foreach (var data in dataHent)
-            {
-                data1.Add(data.StartDateTime);
-                data2.Add(data.WindSpeed);
-                data3.Add(data.Humidity);
-
-            }
-
-            Chart = new Chart(width: 800, height: 400)
-                .AddTitle("Data")
-                .AddSeries(
-                    name: "WindsSpeeds",
-                    chartType: "Line",
-                    xValue: data1.ToArray(),
-                    yValues: data2.ToArray()
-                    )
-
-                .AddSeries(
-                    name: "Temperature",
-                    chartType: "Line",
-                    xValue: data1.ToArray(),
-                    yValues: data3.ToArray());
-        }
-
+        // This method creates and set our chart.
         [HttpPost, ActionName("ChartBool")]
         public ActionResult ChartboolActionResult(ChartViewModel viewModel)
         {
-            var HentData = db.MeteorologiData.Take(500);
+            // Get data for our chart
+            var dbData = db.MeteorologiData.Take(100);
 
-             Chart = new Chart(width: 800, height: 400);
+            // Instantiate our chart
+            Chart = new Chart(width: 800, height: 400);
 
+            // Prepare lists to contain the different data
             var DataWindSpeed = new List<double?>();
             var DataWindDirection = new List<double?>();
             var DataHumidity = new List<double?>();
@@ -175,7 +58,8 @@ namespace DbGraf2.Controllers
             var DataRadiation = new List<double?>();
             var DataDateTime = new List<DateTime?>();
 
-            foreach (var data in HentData)
+            // Populate the lists with our db data
+            foreach (var data in dbData)
             {
                 DataWindSpeed.Add(data.WindSpeed);
                 DataWindDirection.Add(data.WindDirection);
@@ -184,41 +68,24 @@ namespace DbGraf2.Controllers
                 DataPressure.Add(data.Pressure);
                 DataRadiation.Add(data.Radiation);
                 DataDateTime.Add(data.StartDateTime);
-
-
-
             }
+
+            // We add data to the chart according to checkboxes checked off
             if (viewModel.ShowWindSpeed)
-            {
                 Chart.AddSeries(xValue: DataDateTime, yValues: DataWindSpeed.ToArray());
-               
-            }
             if (viewModel.ShowWindDirection)
-            {
                 Chart.AddSeries(xValue: DataDateTime, yValues: DataWindDirection.ToArray());
-            }
             if (viewModel.ShowHumidity)
-            {
                 Chart.AddSeries(xValue: DataDateTime, yValues: DataHumidity.ToArray());
-            }
             if (viewModel.ShowTemperature)
-            {
                 Chart.AddSeries(xValue: DataDateTime, yValues: DataTemperature.ToArray());
-            }
             if (viewModel.ShowPressure)
-            {
                 Chart.AddSeries(xValue: DataDateTime, yValues: DataPressure.ToArray());
-            }
             if (viewModel.ShowRadiation)
-            {
                 Chart.AddSeries(xValue: DataDateTime, yValues: DataRadiation.ToArray());
-            }
 
-
+            // The chart has been created and populated with data, so we return to the index view
             return RedirectToAction("Index");
-
-
-
         }
 
         //protected override void Dispose(bool disposing)
